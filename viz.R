ec96a998e2237f0a9712cf2fc8c50c1628e12e27
Katherine Treadwell <ktread@KatherinesAir3.hsd1.co.comcast.net>
2019-07-31 11:58:55 -0600

    .

--- viz.R
+++ viz.R
@@ -1,3 +1,4 @@
+library(googleVis)
 hist <- gvisHistogram(poverty, options = list(title= "Poverty Rates", 
                                               titleTextStyle="{position: 'top', textStyle: {color: 'blue', fontSize: 26}}",
                                               tooltip= "count",
